3924be6d331fbe1a9835a159e4916ebe26651a5f
debizson <dinnyes2@freemail.hu>
2025-06-11 13:45:02 +0200

    second commit

--- SHCB.m
+++ SHCB.m
@@ -1,7 +1,11 @@
-function [outputArg1,outputArg2] = SHCB(inputArg1,inputArg2)
+function [f,g,H] = SHCB(x)
 %SHCB Summary of this function goes here
 %   Detailed explanation goes here
+
+f=4*x(1)^2-2.1*x(1)^4+1/3*x(1)^6+x(1)*x(2)-4*x(2)^2+4*x(2)+4;
+
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
-end
 
+%
+end
